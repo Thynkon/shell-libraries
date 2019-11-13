0a6d165cb09d9d61cd8b9a0314387ad04ade6bdb
Mario Ferreira <marioferreira2110@gmail.com>
2019-11-13 22:14:09 +0100

    Add lib_log.zsh and lib_string.zsh for the first time

---
+++ lib_log.zsh
@@ -0,0 +1,36 @@
+#!/usr/bin/env zsh
+
+# Import library that has a function that checks if a string
+# contains a substring
+. $HOME/dev/shell-libraries/lib_string.zsh
+
+current_shell=$(ps -p $$ | awk 'FNR == 2 {print $4}')
+
+# In zsh, arrays start from 1 instead of 0
+# This variable allows the script to be compatible with zsh and bash
+ARRAY_START=0
+
+# Using the constains function because in macOs, the current_shell
+# variable is something like "-zsh"
+contains $current_shell "zsh" && ARRAY_START=1
+
+readonly LOG_EMERG=ARRAY_START
+readonly LOG_ALERT=$((LOG_EMERG+1))
+readonly LOG_CRIT=$((LOG_ALERT+1))
+readonly LOG_ERR=$((LOG_CRIT+1))
+readonly LOG_WARNING=$((LOG_ERR+1))
+readonly LOG_NOTICE=$((LOG_WARNING+1))
+readonly LOG_INFO=$((LOG_NOTICE+1))
+readonly LOG_DEBUG=$((LOG_INFO+1))
+
+# Set default verbose level
+verbose_level=$LOG_ERR
+
+.log() {
+	local LOG_LEVELS=("emerg" "alert" "crit" "err" "warning" "notice" "info" "debug")
+	local LEVEL=${1}
+
+	if [ $verbose_level -ge $LEVEL ]; then
+		echo "[${LOG_LEVELS[$LEVEL]}]" "$2"
+	fi
+}
---
+++ lib_string.zsh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+
+# Reference: https://stackoverflow.com/a/8811800
+# contains(string, substring)
+#
+# Returns 0 if the specified string contains the specified substring,
+# otherwise returns 1.
+contains() {
+    string="$1"
+    substring="$2"
+    if test "${string#*$substring}" != "$string"
+    then
+        return 0    # $substring is in $string
+    else
+        return 1    # $substring is not in $string
+    fi
+}
